6713ec16a070acf36ca69cae2bf22508b0be3ae5
serzhBarmin <barminsergeialex@gmail.com>
2023-08-01 13:54:39 +0530

    add default django env

---
+++ dja_barm_pr/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dja_barm_pr project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dja_barm_pr.settings')
+
+application = get_asgi_application()
