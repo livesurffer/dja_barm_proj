12b795a5e0dfa97e7892432921f294f44ecb851d
serzhBarmin <barminsergeialex@gmail.com>
2023-08-01 15:00:44 +0530

    hmls files added

--- dja_barm_pr/settings.py
+++ dja_barm_pr/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dir_app.apps.DirAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'dja_barm_pr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
